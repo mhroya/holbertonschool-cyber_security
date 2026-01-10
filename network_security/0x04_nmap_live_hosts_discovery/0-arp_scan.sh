0a6bd47f779078560033da512eb70f7b721a6247
mhroya <ryamaharram@gmail.com>
2026-01-10 08:43:26 -0500

    oth task

--- network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
+++ network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-if [ -z "$1" ]; then
-  echo "Usage: $0 <subnetwork>"
-  exit 1
-fi
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root or with sudo"
-  exit 1
-fi
-nmap -PR -sn "$1"
+sudo nmap -PR -sn "$1"
